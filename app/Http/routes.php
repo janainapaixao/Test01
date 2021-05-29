4dfd50c40da3cc23c5c527746acc9c898a4d0379
Janaína <janainap@gmail.com>
2021-05-29 14:51:28 -0300

    .

---
+++ app/Http/routes.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::get('/', 'PeopleController@index');
--- app/Models/People.php
+++ app/Models/People.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class People extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'people';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'people_id';
+
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    const CREATED_AT = 'creation_date';
+    const UPDATED_AT = 'updated_date';
+
+    /**
+     * The database connection that should be used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'sqlite';
+
 }
